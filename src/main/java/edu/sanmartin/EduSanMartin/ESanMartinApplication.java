26e3e8c4aaa18065c4673815aff0c1ae37ddd5c4
htr999999999 <racyzed@gmail.com>
2020-06-14 05:34:48 -0500

    Inicio de proyecto, agregado: html/css, models, services, controllers

---
+++ src/main/java/edu/sanmartin/EduSanMartin/ESanMartinApplication.java
@@ -0,0 +1,13 @@
+package edu.sanmartin.EduSanMartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESanMartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESanMartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sanmartin/EduSanMartin/ESanMartinApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sanmartin.EduSanMartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESanMartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
